941130ddc83e32ef1cece0422fdba570bbb3d32d
duoduoeeee <pleasant3518@126.com>
2017-12-24 01:27:26 +0800

    changelog
    修复了显示卡片时 iframe 无法自动缩放的 bug

--- embed.php
+++ embed.php
@@ -13,6 +13,7 @@ echo '
 <meta charset="utf-8">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 <meta name="referrer" content="no-referrer" />
+<script type="application/javascript" src="iframeResizer.contentWindow.min.js"></script>
 </head>
 <body class="embed">
 ';
